aad0066e3fc942762d0046dcdc7b0e1b6864f603
Paal Markus Bjørnstad <paamb@stud.ntnu.no>
2022-03-25 14:21:50 +0100

    Testing

--- db.py
+++ db.py
@@ -2,7 +2,7 @@ import sqlite3
 from os import system, name
 
 DATABASE = "prosjekt_db_innlevering1.db"
-
+cursor = None
 def clear_terminal():
 	if name == 'nt':
 		_ = system('cls')
@@ -31,29 +31,6 @@ def fill_database():
 
 	cursor = con.cursor()
 
-	cursor.execute('''INSERT OR IGNORE INTO Bonneparti VALUES (1, 2021, 8, 1, 1)''')
-	cursor.execute('''INSERT OR IGNORE INTO Bonneparti VALUES (2, 2022, 10, 2, 2)''')
-	cursor.execute('''INSERT OR IGNORE INTO Bonneparti VALUES (3, 2022, 10, 3, 1)''')
-	cursor.execute('''INSERT OR IGNORE INTO Bonneparti VALUES (4, 2022, 10, 4, 2)''')
-	cursor.execute('''INSERT OR IGNORE INTO Bonneparti VALUES (5, 2022, 10, 5, 1)''')
-	cursor.execute('''INSERT OR IGNORE INTO Bonneparti VALUES (6, 2022, 10, 6, 2)''')
-
-	cursor.execute('''INSERT OR IGNORE INTO BonneFraGard VALUES (1, 1)''')
-	cursor.execute('''INSERT OR IGNORE INTO BonneFraGard VALUES (2, 1)''')
-
-	cursor.execute('''INSERT OR IGNORE INTO BonnerIParti VALUES (1, 1)''')
-	cursor.execute('''INSERT OR IGNORE INTO BonnerIParti VALUES (1, 2)''')
-	cursor.execute('''INSERT OR IGNORE INTO BonnerIParti VALUES (2, 2)''')
-	cursor.execute('''INSERT OR IGNORE INTO BonnerIParti VALUES (3, 1)''')
-	cursor.execute('''INSERT OR IGNORE INTO BonnerIParti VALUES (4, 2)''')
-	cursor.execute('''INSERT OR IGNORE INTO BonnerIParti VALUES (4, 1)''')
-	cursor.execute('''INSERT OR IGNORE INTO BonnerIParti VALUES (5, 1)''')
-	cursor.execute('''INSERT OR IGNORE INTO BonnerIParti VALUES (6, 2)''')
-
-	cursor.execute('''INSERT OR IGNORE INTO Bruker VALUES (1, 'logan_paal@epost.no', 'superweakpw123', 'Paal Markus Bjørnstad')''')
-	cursor.execute('''INSERT OR IGNORE INTO Bruker VALUES (2, 'torstein@epost.no', 'superbadpw123', 'Torstein Korten')''')
-	cursor.execute('''INSERT OR IGNORE INTO Bruker VALUES (3, 'odd@epost.no', 'superstrongpw123', 'Odd Magne Gynnild')''')
-
 	cursor.execute('''INSERT OR IGNORE INTO Foredlingsmetode VALUES (1, 'Bærtørket', 'Naar man bærtørker saa faar kaffen en helt spesiell aroma')''')
 	cursor.execute('''INSERT OR IGNORE INTO Foredlingsmetode VALUES (2, 'Vasket', 'Denne kan gjøre at bønnen kan føles litt kjedelig ut')''')
 
@@ -64,27 +41,69 @@ def fill_database():
 	cursor.execute('''INSERT OR IGNORE INTO Gard VALUES (5, 'Nombre de Seis', '1669', 'Wales', 'Kanto')''')
 	cursor.execute('''INSERT OR IGNORE INTO Gard VALUES (6, 'Nombre de Siete', '1337', 'Wales', 'Johto')''')
 
+	# Ikke endre
 	cursor.execute('''INSERT OR IGNORE INTO Kaffebonne VALUES (1, 'coffea arabica', 'arabica')''')
 	cursor.execute('''INSERT OR IGNORE INTO Kaffebonne VALUES (2, 'coffea robusta', 'brabonneart')''')
 
-	cursor.execute('''INSERT OR IGNORE INTO Kaffebrenneri VALUES (1, 'Jacobsen & Svart')''')
+	#Endre ?
+	cursor.execute('''INSERT OR IGNORE INTO BonneFraGard VALUES (1, 1)''')
+	cursor.execute('''INSERT OR IGNORE INTO BonneFraGard VALUES (2, 1)''')
 
-	cursor.execute('''INSERT OR IGNORE INTO Kaffesmaking VALUES (1, 'Veldig god kaffe', 10 ,'2022-02-02', 1, 1)''')
+	# Laget
+	cursor.execute('''INSERT OR IGNORE INTO BonnerIParti VALUES (1, 1)''')
+	cursor.execute('''INSERT OR IGNORE INTO BonnerIParti VALUES (1, 2)''')
+	cursor.execute('''INSERT OR IGNORE INTO BonnerIParti VALUES (2, 2)''')
+	cursor.execute('''INSERT OR IGNORE INTO BonnerIParti VALUES (3, 1)''')
+	cursor.execute('''INSERT OR IGNORE INTO BonnerIParti VALUES (4, 2)''')
+	cursor.execute('''INSERT OR IGNORE INTO BonnerIParti VALUES (4, 1)''')
+	cursor.execute('''INSERT OR IGNORE INTO BonnerIParti VALUES (5, 1)''')
+	cursor.execute('''INSERT OR IGNORE INTO BonnerIParti VALUES (6, 2)''')
+
+	cursor.execute('''INSERT OR IGNORE INTO Bonneparti VALUES (1, 2009, 8, 1, 1)''')
+	cursor.execute('''INSERT OR IGNORE INTO Bonneparti VALUES (2, 2013, 10, 2, 2)''')
+	cursor.execute('''INSERT OR IGNORE INTO Bonneparti VALUES (3, 2015, 10, 3, 1)''')
+	cursor.execute('''INSERT OR IGNORE INTO Bonneparti VALUES (4, 2012, 10, 4, 2)''')
+	cursor.execute('''INSERT OR IGNORE INTO Bonneparti VALUES (5, 2015, 10, 5, 1)''')
+	cursor.execute('''INSERT OR IGNORE INTO Bonneparti VALUES (6, 2017, 10, 6, 2)''')
+
+
+# Ikke endre
+	cursor.execute('''INSERT OR IGNORE INTO Bruker VALUES (1, 'logan_paal@epost.no', 'superweakpw123', 'Paal Markus Bjørnstad')''')
+	cursor.execute('''INSERT OR IGNORE INTO Bruker VALUES (2, 'torstein@epost.no', 'superbadpw123', 'Torstein Korten')''')
+	cursor.execute('''INSERT OR IGNORE INTO Bruker VALUES (3, 'odd@epost.no', 'superstrongpw123', 'Odd Magne Gynnild')''')
+
+# Ikke endre
+
+
+	# Ikke endre
+
+
+	# Lag flere
+	cursor.execute('''INSERT OR IGNORE INTO Kaffebrenneri VALUES (1, 'Jacobsen & Svart')''')
+	cursor.execute('''INSERT OR IGNORE INTO Kaffebrenneri VALUES (2, 'The Barn')''')
+	cursor.execute('''INSERT OR IGNORE INTO Kaffebrenneri VALUES (2, 'Coffee Supreme')''')
+
+	# Brennerid - Partiid
+	cursor.execute('''INSERT OR IGNORE INTO Kaffe VALUES (1, 'Høstkaffe 2019', 'Hint av aprikos', 100 , '2022-20-01', 'middels', 1, 1)''')
+	cursor.execute('''INSERT OR IGNORE INTO Kaffe VALUES (2, 'San Pedro', 'Smak av karamell', 500000 , '2022-01-01', 'mørk', 2, 2)''')
+	cursor.execute('''INSERT OR IGNORE INTO Kaffe VALUES (3, 'Sommerkaffe 2021', 'Kjempeflott', 200 , '2021-02-02', 'lys', 3, 2)''')
+	cursor.execute('''INSERT OR IGNORE INTO Kaffe VALUES (4, 'Sommerkaffe 2022', 'Floral i smaken', 200 , '2021-02-02', 'lys', 1, 2)''')
+	cursor.execute('''INSERT OR IGNORE INTO Kaffe VALUES (5, 'Sun Drop', 'Smak av appelsin og sjasmin', 200 , '2021-02-02', 'lys', 2, 2)''')
+	cursor.execute('''INSERT OR IGNORE INTO Kaffe VALUES (6, 'Boxer Blend', 'Fruktig og floral', 200 , '2021-02-02', 'lys', 3, 3)''')
+	cursor.execute('''INSERT OR IGNORE INTO Kaffe VALUES (7, 'Sommerkaffe 2020', 'Sommerlig', 200 , '2021-02-02', 'lys', 1, 4)''')
+	cursor.execute('''INSERT OR IGNORE INTO Kaffe VALUES (8, 'Our Plot Daterra', 'Vanilje og pære', 200 , '2021-02-02', 'lys', 2, 5)''')
+	cursor.execute('''INSERT OR IGNORE INTO Kaffe VALUES (9, 'Magnifique', 'Mørk', 200 , '2021-02-02', 'lys', 3, 6)''')
+
+
+	# Brukerid - Kaffeid
+	cursor.execute('''INSERT OR IGNORE INTO Kaffesmaking VALUES (1, 'Aprikosen i kaffen gjør denne veldig god', 10 ,'2022-02-02', 1, 1)''')
 	cursor.execute('''INSERT OR IGNORE INTO Kaffesmaking VALUES (2, 'Wow – en odyssé for smaksløkene: sitrusskall, melkesjokolade, aprikos!', 10 ,'2022-03-02', 1, 2)''')
-	cursor.execute('''INSERT OR IGNORE INTO Kaffesmaking VALUES (3, 'Wow – en odyssé for smaksløkene: sitrusskall, melkesjokolade, aprikos!', 10 ,'2022-03-02', 2, 2)''')
-	cursor.execute('''INSERT OR IGNORE INTO Kaffesmaking VALUES (4, 'Wow – en odyssé for smaksløkene: sitrusskall, melkesjokolade, aprikos!', 10 ,'2022-03-02', 3, 2)''')
-	cursor.execute('''INSERT OR IGNORE INTO Kaffesmaking VALUES (5, 'Vasket!', 10 ,'2022-03-02', 3, 2)''')
-	cursor.execute('''INSERT OR IGNORE INTO Kaffesmaking VALUES (6, 'En opplevelse av rang, floral!', 10 ,'2022-03-02', 3, 3)''')
-	cursor.execute('''INSERT OR IGNORE INTO Kaffesmaking VALUES (7, 'En opplevelse av rang, floral!', 10 ,'2022-03-02', 3, 6)''')
-	cursor.execute('''INSERT OR IGNORE INTO Kaffe VALUES (1, 'Vaar 2022', 'Veldig god', 100 , '2022-20-01', 'middels', 1, 1)''')
-	cursor.execute('''INSERT OR IGNORE INTO Kaffe VALUES (2, 'Sommerkaffe 2022', 'Veldig god', 500000 , '2022-20-01', 'mørk', 1, 2)''')
-	cursor.execute('''INSERT OR IGNORE INTO Kaffe VALUES (3, 'Sommerkaffe 2021', 'Kjempeflott', 200 , '2021-02-02', 'lys', 1, 2)''')
-	cursor.execute('''INSERT OR IGNORE INTO Kaffe VALUES (4, 'Sommerkaffe 2022', 'Kjempeflott floral i smaken', 200 , '2021-02-02', 'lys', 1, 2)''')
-	cursor.execute('''INSERT OR IGNORE INTO Kaffe VALUES (5, 'Sommerkaffe 2023', 'Kjempeflott', 200 , '2021-02-02', 'lys', 1, 2)''')
-	cursor.execute('''INSERT OR IGNORE INTO Kaffe VALUES (6, 'Sommerkaffe 2024', 'Kjempeflott', 200 , '2021-02-02', 'lys', 1, 3)''')
-	cursor.execute('''INSERT OR IGNORE INTO Kaffe VALUES (7, 'Sommerkaffe 2025', 'Kjempeflott', 200 , '2021-02-02', 'lys', 1, 4)''')
-	cursor.execute('''INSERT OR IGNORE INTO Kaffe VALUES (8, 'Sommerkaffe 2026', 'Kjempeflott', 200 , '2021-02-02', 'lys', 1, 5)''')
-	cursor.execute('''INSERT OR IGNORE INTO Kaffe VALUES (9, 'Sommerkaffe 2027', 'Kjempeflott', 200 , '2021-02-02', 'lys', 1, 6)''')
+	cursor.execute('''INSERT OR IGNORE INTO Kaffesmaking VALUES (3, 'Fruktig og floral men gir meg ikke noe mer', 10 ,'2022-03-02', 2, 3)''')
+	cursor.execute('''INSERT OR IGNORE INTO Kaffesmaking VALUES (4, 'Litt for sterk', 10 ,'2022-03-02', 3, 4)''')
+	cursor.execute('''INSERT OR IGNORE INTO Kaffesmaking VALUES (5, 'Kjenner at denne kaffen er vasket, noe jeg ikke er noe glad i', 10 ,'2022-03-02', 3, 5)''')
+	cursor.execute('''INSERT OR IGNORE INTO Kaffesmaking VALUES (6, 'En opplevelse av rang, floral!', 10 ,'2022-03-02', 3, 6)''')
+	cursor.execute('''INSERT OR IGNORE INTO Kaffesmaking VALUES (7, 'En veldig god kaffe', 10 ,'2022-03-02', 3, 7)''')
+	
 
 	con.commit()
 	con.close()
@@ -115,54 +134,71 @@ def brukerhistorie_1(bruker):
 	cursor.execute('''INSERT OR IGNORE INTO Kaffebrenneri VALUES (1, 'Jacobsen & Svart')''')
 	cursor.execute('''INSERT OR IGNORE INTO Kaffe VALUES (1, 'Vinterkaffe 2022', 'Ingen beskrivelse', 600 , '2022-20-01', 'lys', 1, 1)''')
 
+	con.commit()
+	con.close()
 
 
 # Brukerhistorie 2
 def brukerhistorie_2():
+	con = sqlite3.connect(DATABASE)
+	cursor = con.cursor()
 	print("    Fullt navn    ||     Antall smakstester    ")
 	print("===============================================")
-	for row in cursor.execute('''SELECT fulltNavn, count(*) as smakstester
-	FROM (
-	SELECT DISTINCT fulltNavn, kaffeId 
-	FROM Bruker INNER JOIN Kaffesmaking ON Bruker.id = Kaffesmaking.brukerId
-	)
-	Group by fulltNavn
-	Order by smakstester desc'''):
+	for row in cursor.execute(
+		'''SELECT fulltNavn, count(*) as smakstester
+		FROM (
+			SELECT DISTINCT fulltNavn, kaffeId 
+			FROM Bruker INNER JOIN Kaffesmaking ON Bruker.id = Kaffesmaking.brukerId
+			WHERE date('now','start of year') <= date(Kaffesmaking.smaksdato)
+		)
+		Group by fulltNavn
+		Order by smakstester desc'''):
 		print(f'  {row[0] : <30}{row[1] : <30}  ')
+	con.commit()
+	con.close()
 
 def brukerhistorie_3():
+	con = sqlite3.connect(DATABASE)
+	cursor = con.cursor()
 	print("    Gjennomsnittspoeng    ||     Kaffenavn     ||     KiloprisNOK     ||     Brennerinavn     ")
 	print("==============================================================================================")
-	for row in cursor.execute('''SELECT AVG(Kaffesmaking.poeng) as avgPoeng, Kaffe.navn, Kaffe.kiloprisNOK, Kaffebrenneri.navn
-FROM Kaffesmaking INNER JOIN Kaffe on (Kaffesmaking.kaffeId = Kaffe.id)
-INNER JOIN Kaffebrenneri on (Kaffe.brenneriId = Kaffebrenneri.id)
-GROUP BY Kaffe.id
-ORDER BY (avgPoeng/Kaffe.kiloprisNOK) DESC'''):
+	for row in cursor.execute('''
+		SELECT AVG(Kaffesmaking.poeng) as avgPoeng, Kaffe.navn, Kaffe.kiloprisNOK, Kaffebrenneri.navn
+		FROM Kaffesmaking INNER JOIN Kaffe on (Kaffesmaking.kaffeId = Kaffe.id)
+		INNER JOIN Kaffebrenneri on (Kaffe.brenneriId = Kaffebrenneri.id)
+		GROUP BY Kaffe.id
+		ORDER BY (avgPoeng/Kaffe.kiloprisNOK) DESC'''):
 		print(f'  {row[0] : <25}{row[1] : <25}{row[2] : <25}{row[3] : <25}  ')
+	con.commit()
+	con.close()
 
 
 # Brukerhistorie 4
 def brukerhistorie_4(filter):
-
+	con = sqlite3.connect(DATABASE)
+	cursor = con.cursor()
 	print("    Brennerinavn    ||     Kaffenavn     ")
 	print("=========================================")
 
-	for row in cursor.execute(f'''SELECT Kaffebrenneri.navn as brennerinavn, Kaffe.navn as kaffenavn
-	FROM Kaffe INNER JOIN Kaffebrenneri on(Kaffe.brenneriId = Kaffebrenneri.id)
-	WHERE Kaffe.beskrivelse like ( ? )
-	UNION
-	SELECT Kaffebrenneri.navn as brennrinavn, Kaffe.navn as kaffenavn
-	FROM Kaffesmaking INNER JOIN Kaffe on (Kaffesmaking.kaffeId = Kaffe.id) INNER JOIN Kaffebrenneri on (Kaffebrenneri.id = Kaffe.brenneriId)
-	WHERE Kaffesmaking.smaksnotat like ( ? )''', [filter, filter]):
+	for row in cursor.execute(f'''
+		SELECT Kaffebrenneri.navn as brennerinavn, Kaffe.navn as kaffenavn
+		FROM Kaffe INNER JOIN Kaffebrenneri on(Kaffe.brenneriId = Kaffebrenneri.id)
+		WHERE Kaffe.beskrivelse like ( ? )
+		UNION
+		SELECT Kaffebrenneri.navn as brennrinavn, Kaffe.navn as kaffenavn
+		FROM Kaffesmaking INNER JOIN Kaffe on (Kaffesmaking.kaffeId = Kaffe.id) INNER JOIN Kaffebrenneri on (Kaffebrenneri.id = Kaffe.brenneriId)
+		WHERE Kaffesmaking.smaksnotat like ( ? )''', [filter, filter]):
 		print(f'  {row[0] : <25}{row[1] : ^8}  ')
-
+	con.commit()
+	con.close()
 	# brukerhistorie_4_output= cursor.fetchall()
 	# print(brukerhistorie_4_output)
 
 
 # Brukerhistorie 5
 def brukerhistorie_5(filter, nasjoner):
-
+	con = sqlite3.connect(DATABASE)
+	cursor = con.cursor()
 	brukerhistorie_input = [filter] + nasjoner
 	# print(nasjoner)
 	# Denne bruker f string men tror at ? gjør at det går fint
@@ -170,21 +206,24 @@ def brukerhistorie_5(filter, nasjoner):
 	print("    Brennerinavn    ||     Kaffenavn     ")
 	print("=========================================")
 
-	for row in cursor.execute(f'''SELECT  Kaffebrenneri.navn AS "brenninavn", Kaffe.navn AS "kaffenavn"
-	FROM Bonneparti INNER JOIN
-	(SELECT *
-	FROM Foredlingsmetode
-	WHERE Foredlingsmetode.id NOT IN(SELECT Foredlingsmetode.id
+	for row in cursor.execute(f'''
+		SELECT  Kaffebrenneri.navn AS "brenninavn", Kaffe.navn AS "kaffenavn"
+		FROM Bonneparti INNER JOIN
+		(SELECT *
 		FROM Foredlingsmetode
-		WHERE Foredlingsmetode.navn like ( ? )
-		)) FiltrertForedlingsmetode
-	ON (FiltrertForedlingsmetode.id = Bonneparti.foredlingsmetodeId)
-	INNER JOIN Gard on (Gard.id = Bonneparti.gardId)
-	INNER JOIN Kaffe on (Kaffe.partiId = Bonneparti.id)
-	INNER JOIN Kaffebrenneri on (Kaffe.brenneriId = Kaffebrenneri.id)
-	WHERE Gard.land IN ({','.join('?' for _ in brukerhistorie_input[1::])})
-	''', (brukerhistorie_input)):
+		WHERE Foredlingsmetode.id NOT IN(SELECT Foredlingsmetode.id
+			FROM Foredlingsmetode
+			WHERE Foredlingsmetode.navn like ( ? )
+			)) FiltrertForedlingsmetode
+		ON (FiltrertForedlingsmetode.id = Bonneparti.foredlingsmetodeId)
+		INNER JOIN Gard on (Gard.id = Bonneparti.gardId)
+		INNER JOIN Kaffe on (Kaffe.partiId = Bonneparti.id)
+		INNER JOIN Kaffebrenneri on (Kaffe.brenneriId = Kaffebrenneri.id)
+		WHERE Gard.land IN ({','.join('?' for _ in brukerhistorie_input[1::])})
+		''', (brukerhistorie_input)):
 		print(f'  {row[0] : <25}{row[1] : ^10}  ')
+	con.commit()
+	con.close()
 
 def velg_brukerhistorie():
 	print('''VELG BRUKERHISTORIE\n
@@ -214,10 +253,9 @@ Brukerhistirie 5: Søk etter kaffe fra land og med foredlingsmetode\n\n''')
 		
 		
 	elif brukerhistorie == 5:
-		print("Skriv inn nasjonene du vil finne kaffe fra. Skriv mellomrom mellom hvert land (Colombia Nigeria Peru): ", end="")
+		print("Skriv inn nasjonene du vil finne kaffe fra. Skriv mellomrom mellom hvert land (Colombia Rwanda Wales): ", end="")
 		nasjoner = [str(nasjon) for nasjon in input().split()]
 		filter = input("Hvilken foredlingsmetode vil du \x1B[3mIKKE\x1B[0m se: ")
-		brukerhistori_input = [filter] + nasjoner
 		clear_terminal()
 		brukerhistorie_5(filter, nasjoner)
 		
@@ -227,25 +265,17 @@ Brukerhistirie 5: Søk etter kaffe fra land og med foredlingsmetode\n\n''')
 		print("Ugyldig input. Skriv inn et tall mellom 1 og 5")
 	input("\n\n\n\nTrykk ENTER for å gå videre")
 
-while True:
-	con = sqlite3.connect(DATABASE)
-	cursor = con.cursor()
-	
-	tabell = input("Vil du fylle tabellene eller tømme de (t/f/ ): ")
-	if tabell == "f":
-		fill_database()
-	elif tabell == "t":
+def main():
+	while True:
+		tabell = input("Vil du fylle tabellene eller tømme de (t/f/ ): ")
+		if tabell == "f":
+			fill_database()
+		elif tabell == "t":
+			clean_database()
+		clear_terminal()
+		velg_brukerhistorie()
+		clear_terminal()
+		#Commiter endringer til databasen
 		clean_database()
-	
-	clear_terminal()
 
-	velg_brukerhistorie()
-
-	clear_terminal()
-	con.commit()
-	# Close connection
-	con.close()
-	#Commiter endringer til databasen
-	clean_database()
-
-
+main()
